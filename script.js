d8c6fd3bf5f1586bbaa0768f26df9aecb250395e
lkkrd <leshkord@gmail.com>
2022-11-03 19:52:31 +0100

    poprawion layout

--- script.js
+++ script.js
@@ -3,34 +3,12 @@ const reset = document.querySelector("#reset");
 
 reset.addEventListener("click", function () {
   btns.forEach((btn) => {
-    btn.style.backgroundColor = "white";
+    btn.style.backgroundColor = "var(--main-color)";
   });
 });
 
-/*btns.forEach((btn) => {
-  btn.addEventListener("click", function () {
-    if (btn.classList.contains("green")) {
-      console.log("green");
-    }
-    if (btn.classList.contains("yellow")) {
-      console.log("yellow");
-    }
-    if (btn.classList.contains("red")) {
-      console.log("red");
-    }
-    if (btn.classList.contains("blue")) {
-      console.log("blue");
-    }
-    if (btn.classList.contains("purple")) {
-      console.log("purple");
-    }
-  });
-}); */
-
 btns.forEach((btn) => {
   btn.addEventListener("click", function () {
-    if (btn.classList.contains(btn.classList[1])) {
-      btn.style.backgroundColor = btn.classList[1];
-    }
+    btn.style.backgroundColor = btn.id;
   });
 });
